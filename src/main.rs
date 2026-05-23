e205df15bec1a0d800db732e31cff3f876a10e78
Mario Klebsch <mario@klebsch.de>
2026-05-23 12:21:41 +0200

    Moved common implementations to trait XmlContainer and added new methods for cdata, comment and doctype

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,42 @@
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 use actix_web::web::Bytes;
-use quick_xml::events::{BytesDecl, BytesStart, BytesEnd, BytesText, BytesPI, Event};
+use quick_xml::events::{BytesDecl, BytesStart, BytesEnd, BytesText, BytesPI, BytesCData, Event};
 use quick_xml::events::attributes::Attribute;
 use quick_xml::Writer;
 use std::io::Cursor;
 
 trait XmlContainer<W: std::io::Write> {
     fn writer(&mut self) -> &mut Writer<W>;
-    fn tag<'a>(&'a mut self, tag_name: &str) -> XmlTag<'a, W>;
+
+    fn tag<'a>(&'a mut self, tag_name: &str) -> XmlTag<'a, W>{
+        XmlTag::new(self.writer(), tag_name)
+    }
+
     fn tag_with_attrs<'a, I>(&'a mut self, tag_name: &str, attributes: I) -> XmlTag<'a, W>
     where
-        I: IntoIterator<Item = Attribute<'a>>;
-    fn text(&mut self, text: &str) -> &mut Self;
-    fn pi(&mut self, text: &str) -> &mut Self;
+        I: IntoIterator<Item = Attribute<'a>> {
+        XmlTag::new_with_attrs(self.writer(), tag_name, attributes)
+    }
+
+    fn text(&mut self, text: &str) -> &mut Self{
+        self.writer().write_event(Event::Text(BytesText::new(text))).unwrap();
+        self
+    }
+
+    fn pi(&mut self, text: &str) -> &mut Self{
+        self.writer().write_event(Event::PI(BytesPI::new(text))).unwrap();
+        self
+    }
+
+    fn cdata(&mut self, text: &str) -> &mut Self{
+        self.writer().write_event(Event::CData(BytesCData::new(text))).unwrap();
+        self
+    }
+
+    fn comment(&mut self, text: &str) -> &mut Self{
+        self.writer().write_event(Event::Comment(BytesText::new(text))).unwrap();
+        self
+    }
 }
 
 struct XmlTag<'a, W: std::io::Write> {
@@ -47,27 +71,6 @@ impl<'tag, W: std::io::Write> XmlContainer<W> for XmlTag<'tag, W> {
     fn writer(&mut self) -> &mut Writer<W> {
         self.writer_
     }
-
-    fn tag<'a>(&'a mut self, tag_name: &str) -> XmlTag<'a, W> {
-        XmlTag::new(self.writer_, tag_name)
-    }
-
-    fn tag_with_attrs<'a, I>(&'a mut self, tag_name: &str, attributes: I) -> XmlTag<'a, W>
-    where
-        I: IntoIterator<Item = Attribute<'a>>,
-    {
-        XmlTag::new_with_attrs(self.writer_, tag_name, attributes)
-    }
-
-    fn text(&mut self, text: &str) -> &mut Self {
-        self.writer_.write_event(Event::Text(BytesText::new(text))).unwrap();
-        self
-    }
-
-    fn pi(&mut self, text: &str) -> &mut Self {
-        self.writer_.write_event(Event::PI(BytesPI::new(text))).unwrap();
-        self
-    }
 }
 
 struct XmlDocument {
@@ -87,35 +90,15 @@ impl XmlDocument {
         Self { writer_: writer }
     }
 
-    fn writer(&mut self) -> &mut Writer<Cursor<Vec<u8>>> {
-        &mut self.writer_
+    fn doctype(&mut self, text: &str) -> &mut Self {
+        self.writer_.write_event(Event::DocType(BytesText::new(text))).unwrap();
+        self
     }
 }
 
 impl XmlContainer<Cursor<Vec<u8>>> for XmlDocument {
     fn writer(&mut self) -> &mut Writer<Cursor<Vec<u8>>> {
         &mut self.writer_
-    }
-
-    fn tag<'a>(&'a mut self, tag_name: &str) -> XmlTag<'a, Cursor<Vec<u8>>> {
-        XmlTag::new(&mut self.writer_, tag_name)
-    }
-
-    fn tag_with_attrs<'a, I>(&'a mut self, tag_name: &str, attributes: I) -> XmlTag<'a, Cursor<Vec<u8>>> 
-    where
-        I: IntoIterator<Item = Attribute<'a>>,
-    {
-        XmlTag::new_with_attrs(&mut self.writer_, tag_name, attributes)
-    }
-
-    fn text(&mut self, text: &str) -> &mut Self {
-        self.writer_.write_event(Event::Text(BytesText::new(text))).unwrap();
-        self
-    }
-
-    fn pi(&mut self, text: &str) -> &mut Self {
-        self.writer_.write_event(Event::PI(BytesPI::new(text))).unwrap();
-        self
     }
 }
 
@@ -141,12 +124,13 @@ async fn hello() -> impl Responder {
 async fn test() -> impl Responder {
     let mut doc = XmlDocument::new();
     {
+        doc.doctype("root");
         doc.pi("processing instruction");
+        doc.comment("This is a comment");
         let mut root = doc.tag("root");
-        {
-            let mut child = root.tag_with_attrs("child", vec![Attribute::from(("name", "value"))]);
-            child.text("This is a child element with attributes.");
-        }
+        root.cdata("This is a CDATA section");
+        let mut child = root.tag_with_attrs("child", vec![Attribute::from(("name", "value<5"))]);
+        child.text("This is a child element <with> attributes.");
     }
     doc
 }
